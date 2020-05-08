66cf029ce0dd8fac91ed499877c5601721a76c5e
Muskelo <titanioman@mail.ru>
2020-05-08 09:57:41 +0300

    add ban function

--- auth/blueprint.py
+++ auth/blueprint.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, abort, render_template, request
-from flask_security import current_user
+from flask import Blueprint, abort, render_template, request,redirect
+from flask_security import current_user, login_required
 
 from auth.forms import SelectRoleForm
 from db import db
@@ -52,3 +52,30 @@ def read_user(user_id):
                            user=user,
                            form=form
                            )
+
+
+@auth.route('ban_user/<user_id>', methods=['POST'])
+@login_required
+def ban_user(user_id):
+    errors = []
+
+    # search
+
+    user = User.query.filter(User.id == user_id).first()
+
+    if not user:
+        abort(404)
+
+    # access
+    if not current_user.has_role("moder"):
+        errors.append("no access")
+
+    if not errors:
+        if user.active == 0:
+            user.active = 1
+        else:
+            user.active = 0
+
+        db.session.commit()
+
+    return redirect(request.referrer)
